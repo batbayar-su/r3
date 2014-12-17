e784eeb572f7492490af5afbe7cdbad01d3a3012
Batbayar Sukhbaatar <batbayar@ubuntu.(none)>
2014-12-16 21:54:54 -0800

    unknown porp_key fixed

--- app/controllers/reason_documentation_controller.rb
+++ app/controllers/reason_documentation_controller.rb
@@ -3,7 +3,7 @@ class ReasonDocumentationController < ApplicationController
   before_filter :find_project
 
   def index
-    @data = Journal.joins('inner join journal_details on journals.id = journal_details.journal_id').joins('inner join users on journal_details.value = users.id').joins('inner join issues on issues.id = journals.journalized_id').joins('inner join projects on projects.id = issues.project_id').where("journalized_type = 'Issue' and journal_details.property = 'cf' and journal_details.value != '' and projects.identifier = '#{params[:project_id]}'")
+    @data = Journal.joins('inner join journal_details on journals.id = journal_details.journal_id').joins('inner join users on journal_details.value = users.id').joins('inner join issues on issues.id = journals.journalized_id').joins('inner join projects on projects.id = issues.project_id').joins('inner join journal_details as jd2 on journal_details.journal_id = jd2.journal_id and journal_details.prop_key != jd2.prop_key and journal_details.property = jd2.property').where("journalized_type = 'Issue' and journal_details.property = 'cf' and journal_details.value != '' and projects.identifier = '#{params[:project_id]}'")
     if params[:scope]
       if params[:scope] == 'previous'
         @data = @data.where("date_format(journals.created_on, '%Y-%m')=date_format(now() - INTERVAL 1 MONTH, '%Y-%m')")
@@ -15,9 +15,9 @@ class ReasonDocumentationController < ApplicationController
     else
       @data = @data.where("date_format(journals.created_on, '%Y-%m')=date_format(now(), '%Y-%m')")
     end
-    @data_summary = @data.group('uname, journalized_id').order('journal_id, prop_key').select("users.id as user_id, concat(users.firstname, ' ', users.lastname) as uname, journalized_id, count(journalized_id) as faults")
+    @data_summary = @data.group('uname, journalized_id, fname').order('journal_details.journal_id, journal_details.prop_key').select("jd2.value as fname, users.id as user_id, concat(users.firstname, ' ', users.lastname) as uname, journalized_id, count(journalized_id) as faults")
     @data_by_user = @data.group('uname').select("users.id as user_id, concat(users.firstname, ' ', users.lastname) as uname, count(journalized_id) as faults")
-    @data_by_error = @data.joins('inner join journal_details as jd2 on journal_details.journal_id = jd2.journal_id and journal_details.prop_key != jd2.prop_key and journal_details.property = jd2.property and journal_details.prop_key = 1').group('fname').select("jd2.value as fname,count(journalized_id) as faults")
+    @data_by_error = @data.group('fname').select("jd2.value as fname,count(journalized_id) as faults")
 
     respond_to do |format|
       format.html
