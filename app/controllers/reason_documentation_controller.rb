d43536e9913355b84c70bae7c0191649c7369cdc
batbayar <batbayar.su@yahoo.com>
2015-01-28 17:13:50 +0800

    fix for if previous key is wrong

--- app/controllers/reason_documentation_controller.rb
+++ app/controllers/reason_documentation_controller.rb
@@ -3,7 +3,15 @@ class ReasonDocumentationController < ApplicationController
   before_filter :find_project
 
   def index
-    @data = Journal.joins('inner join journal_details on journals.id = journal_details.journal_id').joins('inner join users on journal_details.value = users.id').joins('inner join issues on issues.id = journals.journalized_id').joins('inner join projects on projects.id = issues.project_id').joins('inner join journal_details as jd2 on journal_details.journal_id = jd2.journal_id and journal_details.prop_key != jd2.prop_key and journal_details.property = jd2.property').joins('inner join custom_fields on custom_fields.id = jd2.prop_key').where("journalized_type = 'Issue' and journal_details.property = 'cf' and journal_details.value != '' and custom_fields.name = 'Reason in charge' and projects.identifier = '#{params[:project_id]}'")
+    @data = Journal
+    .joins('inner join journal_details on journals.id = journal_details.journal_id')
+    .joins("inner join custom_fields on custom_fields.id = journal_details.prop_key and custom_fields.name = 'Reason person'")
+    .joins('inner join users on journal_details.value = users.id')
+    .joins('inner join issues on issues.id = journals.journalized_id')
+    .joins('inner join projects on projects.id = issues.project_id')
+    .joins('inner join journal_details as jd2 on journal_details.journal_id = jd2.journal_id and journal_details.prop_key != jd2.prop_key and journal_details.property = jd2.property')
+    .joins("inner join custom_fields as cf2 on cf2.id = jd2.prop_key and cf2.name = 'Reason in charge'")
+    .where("journalized_type = 'Issue' and journal_details.property = 'cf' and journal_details.value != '' and projects.identifier = '#{params[:project_id]}'")
     if params[:scope]
       if params[:scope] == 'previous'
         @data = @data.where("date_format(journals.created_on, '%Y-%m')=date_format(now() - INTERVAL 1 MONTH, '%Y-%m')")
